bb646f78ee3b281eda677d23605d9f78d9376f94
Vaïk STEEG <vaik.steeg@laposte.fr>
2023-10-05 11:05:24 +0200

    Update test_risk_simulation.py
    Ajout tests d'acceptation (valeurs possible et valeurs aux limites

--- test_risk_simulation.py
+++ test_risk_simulation.py
@@ -31,6 +31,21 @@ class Test(unittest.TestCase):
         assert score.get("message") == ""
         
 
+    def test_valide_parameters(self):
+        for genre in range(2):
+            for eval_region in range(1, 4):
+                for ecart_adr in range(2):
+                    for activite_pro in range(2):
+                        for scol_univ in range(2):
+                            for risque_all_loan in [0, 10, 20, 30]:
+                                for current_credit_cb in [0, 10, 20, 30]:
+                                    for nb_mens in [0, 10, 20, 30]:
+                                        for nb_cb in [0, 10, 20, 30]:
+                                            self.test_valide(genre=genre, eval_region=eval_region, ecart_adr=ecart_adr, 
+                                                             activite_pro=activite_pro, scol_univ=scol_univ, 
+                                                            risque_all_loan=risque_all_loan, current_credit_cb=current_credit_cb, 
+                                                            nb_mens=nb_mens, nb_cb=nb_cb)
+
 
     #@unittest.expectedFailure
     def testBadParameters(self, genre: int = 2, eval_region: int = 0, ecart_adr: int = 2, activite_pro: int = 2, scol_univ: int = 2, 
@@ -43,7 +58,22 @@ class Test(unittest.TestCase):
         assert score.get("score_simulation") == 999
         assert score.get("message") != ""
 
-    
+    def test_invalide_parameters(self):
+        
+        for genre in [-1, 0.5, 2]:
+            for eval_region in [-1, 0, 1, 0.5, 5]:
+                for ecart_adr in [-1, 0, 0.5, 2]:
+                    for activite_pro in [-1, 0, 0.5, 2]:
+                        for scol_univ in [-1, 0, 0.5, 2]:
+                            for risque_all_loan in [-1, 0.5, 1, 31]:
+                                for current_credit_cb in [-1, 0.5, 1, 31]:
+                                    for nb_mens in [-1, 0.5, 1, 31]:
+                                        for nb_cb in [-1, 0.5, 1, 31]:
+                                            self.testBadParameters(genre=genre, eval_region=eval_region, ecart_adr=ecart_adr, 
+                                                             activite_pro=activite_pro, scol_univ=scol_univ, 
+                                                            risque_all_loan=risque_all_loan, current_credit_cb=current_credit_cb, 
+                                                            nb_mens=nb_mens, nb_cb=nb_cb)
+
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
